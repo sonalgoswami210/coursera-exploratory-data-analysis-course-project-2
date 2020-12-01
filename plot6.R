9a239d51ad568cee1d1e0b9f1e8b7d4adb5765e0
Sonal Goswami <68932077+sonalgoswami210@users.noreply.github.com>
2020-12-01 13:43:36 +0530

    Create plot6.R

---
+++ plot6.R
@@ -0,0 +1,36 @@
+
+
+SCC <- readRDS("summarySCC_PM25.rds")
+source <- readRDS("Source_Classification_Code.rds")
+
+# merge the two data sets 
+SCCsource <- merge(SCC, source, by="SCC")
+
+library(ggplot2)
+
+# Compare emissions from motor vehicle sources in Baltimore City with emissions from motor 
+# vehicle sources in Los Angeles County, California (fips == "06037"). 
+# Which city has seen greater changes over time in motor vehicle emissions?
+
+
+# 24510 is Baltimore, see plot2.R, 06037 is LA CA
+# Searching for ON-ROAD type in SCC
+
+subsetSCC <- SCC[(SCC$fips=="24510"|SCC$fips=="06037") & SCC$type=="ON-ROAD",  ]
+
+aggregatedTotalByYearAndFips <- aggregate(Emissions ~ year + fips, subsetSCC, sum)
+aggregatedTotalByYearAndFips$fips[aggregatedTotalByYearAndFips$fips=="24510"] <- "Baltimore, MD"
+aggregatedTotalByYearAndFips$fips[aggregatedTotalByYearAndFips$fips=="06037"] <- "Los Angeles, CA"
+
+
+png("plot6.png", width=1040, height=480)
+
+g <- ggplot(aggregatedTotalByYearAndFips, aes(factor(year), Emissions))
+g <- g + facet_grid(. ~ fips)
+g <- g + geom_bar(stat="identity")  +
++     xlab("year") +
++     ylab(expression('Total PM'[2.5]*" Emissions")) +
++     ggtitle('Total Emissions from motor vehicle (type=ON-ROAD) in Baltimore City, MD (fips = "24510") vs Los Angeles, CA (fips = "06037")  1999-2008')
+
+print(g)
+dev.off()
