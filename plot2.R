dab4ebd0f534f793f47fc9edffb02952a9474766
Sonal Goswami <68932077+sonalgoswami210@users.noreply.github.com>
2020-12-01 12:44:27 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,1 +1,15 @@
 ## This first line will likely take a few seconds. Be patient!
+
+SCC <- readRDS("summarySCC_PM25.rds")
+source <- readRDS("Source_Classification_Code.rds")
+
+# Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") from 1999 to 2008? 
+# Use the base plotting system to make a plot answering this question.
+
+subsetSCC  <- SCC[SCC$fips=="24510", ]
+
+aggregatedTotalByYear <- aggregate(Emissions ~ year, subsetSCC, sum)
+png('plot2.png')
+barplot(height=aggregatedTotalByYear$Emissions, names.arg=aggregatedTotalByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' in the Baltimore City, MD emissions at various years'))
+
+dev.off()
